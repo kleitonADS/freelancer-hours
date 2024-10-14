37de09bb87c36394cfab311f037fef72a4724f2e
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2024-10-14 13:38:04 -0300

    add files
    final files project

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,35 @@
 namespace App\Livewire\Projects;
 use App\Models\Project;
 use Livewire\Component;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Computed;
+
+
+
+
+
 
 class Proposals extends Component
 {
 
     public Project $project;
+    public int $qty = 5;
 
+    #[Computed()]
+    public function proposals(){
+        return $this->project->proposals()->orderBy('hours')->paginate($this->qty);
+    }
+
+    public function loadMore(){
+        $this->qty += 5;
+    }
+
+    #[Computed()]
+    public function lastProposalTime(){
+       return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -5,6 +5,7 @@ use App\Models\Project;
 use Livewire\Attributes\Rule;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Illuminate\Support\Facades\DB;
 
 class Create extends Component
 
@@ -33,13 +34,40 @@ class Create extends Component
             return;
         }
 
-        $this->project->proposals()
+        $proposal = $this->project->proposals()
 
         ->updateOrCreate(
 
            [ 'email' => $this->email ],
            [ 'hours' => $this->hours ],
         );
+
+        $this->arrangePosition( $proposal);
+
+
+        $this->dispatch('proposal::created');
+        $this->modal = false;
+
+    }
+
+    public function arrangePosition(Proposal $proposal){
+
+        $query = DB::select("
+
+                select *, row_number() over ( order by hours asc ) as newPosition
+                from proposals
+                where project_id = :project
+
+        ", ['project' => $proposal->project_id]);
+
+        $position = collect($query)->where('id', '=', $proposal->id)->first();
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+
+
+        if($otherProposal) {
+            $proposal->update(['position_status' => 'up']);
+            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status', 'down']);
+        }
 
     }
 
